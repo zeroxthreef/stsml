f7099bed24a01a94b9b6ee16b4dc2b1d2f458673
zeroxthreef <zeroxthreef@gmail.com>
2021-04-01 15:25:54 -0500

    updated to newest STS

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xxd -i -a lib/SimpleTinyScript/stdlib.sts > stdlib.h
-cc -Wall -g -o stsml src/main.c src/parser.c src/util.c lib/SimpleTinyScript/cli.c lib/SimpleTinyScript/ext/pdjson/pdjson.c -lonion -lhiredis -lpthread -lm -DNO_CLI_MAIN=1 -DCOMPILING=1 -DSTS_GOTO_JIT
+cc -Wall -g -o stsml src/main.c src/parser.c src/util.c lib/SimpleTinyScript/cli.c -lonion -lhiredis -lpthread -lm -DNO_CLI_MAIN=1 -DCOMPILING=1 -DSTS_GOTO_JIT
